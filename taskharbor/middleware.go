439201ecd4e5002e45c8a83bec55987592a6ad57
ARJ2211 <aayushrj22@gmail.com>
2026-01-21 16:00:51 -0500

    Added middleware for task handlers

---
+++ taskharbor/middleware.go
@@ -0,0 +1,15 @@
+package taskharbor
+
+import "context"
+
+/*
+Handler will be the function that will process
+the different jobs.
+*/
+type Handler func(ctx context.Context) error
+
+/*
+Middleware will wrap the handler and return a new
+handler. TODO: PANIC REC, TIMEOUTS, RETRIES, etc...
+*/
+type Middleware func(Handler) Handler
