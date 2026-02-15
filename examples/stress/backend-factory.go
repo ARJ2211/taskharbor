ea4beacaacaffd83125f0f116ca9fdac835b5528
ARJ2211 <aayushrj22@gmail.com>
2026-02-15 12:16:59 -0500

    Added backend factory to create a driver based on the configs

---
+++ examples/stress/backend-factory.go
@@ -0,0 +1,37 @@
+// examples/stress/backend_factory.go
+package main
+
+import (
+	"context"
+	"errors"
+	"time"
+
+	"github.com/ARJ2211/taskharbor/taskharbor/driver"
+)
+
+func (b backend) Close() error                    { return b.CloseFn() }
+func (b backend) Reset(ctx context.Context) error { return b.ResetFn(ctx) }
+func (b backend) Progress(ctx context.Context, qs []string, now time.Time) (Progress, error) {
+	return b.ProgFn(ctx, qs, now)
+}
+
+type backend struct {
+	Driver   driver.Driver
+	CloseFn  func() error
+	ResetFn  func(context.Context) error
+	ProgFn   func(context.Context, []string, time.Time) (Progress, error)
+	DriverID string
+}
+
+func newBackend(ctx context.Context, cfg Config) (backend, error) {
+	switch cfg.DriverType {
+	case "postgres":
+		return newPostgresBackend(ctx, cfg)
+	case "redis":
+		return newRedisBackend(ctx, cfg)
+	default:
+		return backend{}, ErrInvalidDriver
+	}
+}
+
+var ErrInvalidDriver = errors.New("invalid driver")
